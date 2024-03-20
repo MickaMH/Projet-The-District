3ccef769ccca139bf431fc9f6d5614576cb2daf6
[MickaMH] <[mika80830@hotmail.fr]>
2024-03-20 16:56:54 +0100

    mer 17.00

--- assets/js/essai_json.js
+++ assets/js/essai_json.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
 
+
+  
 $.getJSON("assets/js/the_district.json", function(data) {
   
     let categories = $("#categories");
@@ -23,7 +25,9 @@ $.getJSON("assets/js/the_district.json", function(data) {
         libelle_categorie.href = "plats_par_categorie.html?id=" + data.categorie[i].id_categorie;
 
 categories.append(element);
+
 element.append(carte_categorie);
+
 carte_categorie.append(image_categorie, libelle_categorie);}});
 
 
@@ -57,43 +61,31 @@ $.getJSON("assets/js/the_district.json", function(data2) {
       commander_plat.href = "commande_json.html?id=" + data2.plat[i].id_plat;
 
 plats.append(element_plats);
+
 element_plats.append(carte_plat);
+
 carte_plat.append(image_plat, libelle_plat, description_plat, commander_plat);}});
 
 
-$.getJSON("assets/js/the_district.json", function(data3) {
-
-  let commande = $("#commande");
-
-  for (i=0; i<data3.plat.length; i++) {
-
-      let element_commande = document.createElement("div");
-      element_commande.className="col-lg-4 p-lg-5 pb-lg-0 mb-3 mt-3";
-
-      let carte_commande = document.createElement("div");
-      carte_commande.className = "card w-100 border-4 bordures rounded-5 ";
-
-      let image_commande = document.createElement("img");
-      image_commande.src = data3.plat[i].image;
-      image_commande.className = "card-img-top p-3 rounded-5";
-
-      let libelle_commande = document.createElement("h5");
-      libelle_commande.textContent = data3.plat[i].libelle;
-      libelle_commande.className = "card-text fs-3 fw-medium ms-3";
-
-      let description_commande = document.createElement("p");
-      description_commande.textContent = data3.plat[i].description;
-      description_commande.className = "card-text fst-italic fw-medium ms-3 me-3";
-
-      let prix_commande = document.createElement("p");
-      prix_commande.textContent = data3.plat[i].prix;
-      prix_commande.className = "card-text fs-5 fw-medium text-center shadow-lg p-2 m-3 mt-0 rounded-4 text-decoration-none fond_logo lettres_blanches";
-
-commande.append(element_commande);
-element_commande.append(carte_commande);
-carte_commande.append(image_commande, libelle_commande, description_commande, prix_commande);}});
 
 
+function recupere_id() {
+
+  let urlParams = new URLSearchParams(window.location.search);
+
+  let plat_id = urlParams.get("id");
+
+  $("#commande").text("ID:" + plat_id);
+  console.log(plat_id);
+}
+
+recupere_id();
+  
+
+    
 });
+
+
+
 
 
