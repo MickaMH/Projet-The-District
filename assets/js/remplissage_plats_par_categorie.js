79a9ec59a86a6605c78a572700e3b836e5185bd5
[MickaMH] <[mika80830@hotmail.fr]>
2024-03-28 17:02:19 +0100

    jeu 17.00

--- assets/js/remplissage_plats_par_categorie.js
+++ assets/js/remplissage_plats_par_categorie.js
@@ -1,56 +1,52 @@
-$(document).ready(function() {
+let full_url = document.URL;
+let url_array = full_url.split('=');
+let categorie_id = url_array[url_array.length - 1];
+console.log(typeof categorie_id, categorie_id);
 
-    $.getJSON("assets/js/the_district.json", function(data4) {
+fetch('assets/js/the_district.json')
+  .then((response) => response.json())
+  .then((data4) => {
 
-    let full_url = document.URL;
+    let data = JSON.parse(data4);
+    // Declare id_categorie
+    let id_categorie = data.id_categorie;
+    console.log(typeof id_categorie, id_categorie);
 
-    let url_array = full_url.split('=');
+    let filtre_plats = data.plat.filter(plat => plat.id_categorie === categorie_id);
 
-    let categorie_id = url_array[url_array.length - 1];
-    console.log(categorie_id);
+    const platsCategorie = document.getElementById("plats_par_categorie");
 
-    let plats_par_categorie = $("#plats_par_categorie");
-
-    let plats_filtre = data4.plat.filter(plat =>
-        plat.id_categorie);
-
-    if (id_categorie === categorie_id) {
-
-        plats_filtre.forEach(plat => {
-  
+    filtre_plats.forEach(plat => {
+        
         let element_plats = document.createElement("div");
-        element_plats.className="col-lg-4 p-lg-5 pb-lg-0 mb-3 mt-3";
-  
+        element_plats.className = "col-lg-4 p-lg-5 pb-lg-0 mb-3 mt-3";
+      
         let carte_plat = document.createElement("div");
-        carte_plat.className = "card w-100 border-4 bordures rounded-5 ";
-  
+        carte_plat.className = "card w-100 border-4 bordures rounded-5";
+      
         let image_plat = document.createElement("img");
-        image_plat.src = data2.plat[i].image;
+        image_plat.src = plat.image;
         image_plat.className = "card-img-top p-3 rounded-5";
-  
+      
         let libelle_plat = document.createElement("h5");
-        libelle_plat.textContent = data2.plat[i].libelle;
+        libelle_plat.textContent = plat.libelle;
         libelle_plat.className = "card-text fs-3 fw-medium ms-3";
-  
+      
         let description_plat = document.createElement("p");
-        description_plat.textContent = data2.plat[i].description;
+        description_plat.textContent = plat.description;
         description_plat.className = "card-text fst-italic fw-medium ms-3 me-3";
-  
+      
         let commander_plat = document.createElement("a");
         commander_plat.textContent = "Commander";
-        commander_plat.className = "card-text fs-5 fw-medium text-center shadow-lg p-2 m-3 mt-0 rounded-4 text-decoration-none fond_logo lettres_blanches";
-        commander_plat.href = "commande_json.html?id=" + data2.plat[i].id_plat;
-  
-    plats_par_categorie.append(element_plats);
-  
-    element_plats.append(carte_plat);
-  
-    carte_plat.append(image_plat, libelle_plat, description_plat, commander_plat);}
+        commander_plat.className = `card-text fs-5 fw-medium text-center shadow-lg p-2 m-3 mt-0 rounded-4 text-decoration-none fond_logo lettres_blanches`;
+        commander_plat.href = `commande_json.html?id=${plat.id_plat}`;
+      
+        platsCategorie.append(element_plats);
+        element_plats.append(carte_plat);
+        carte_plat.append(image_plat, libelle_plat, description_plat, commander_plat);
 
-)}})})
-          
-
-
-
-    
-
+    });
+  })
+  .catch((error) => {
+    console.error('Erreur lors du chargement du fichier JSON :', error);
+  });
